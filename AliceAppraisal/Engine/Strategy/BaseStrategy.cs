ad0c07e481feb401ce59167a32538d5ba54b9bc0
Milovidov Vasya <milovidov.vasya@gmail.com>
2021-01-30 17:47:12 +0300

    add comment

--- AliceAppraisal/Engine/Strategy/BaseStrategy.cs
+++ AliceAppraisal/Engine/Strategy/BaseStrategy.cs
@@ -6,26 +6,28 @@ using System.Threading.Tasks;
 namespace AliceAppraisal.Engine.Strategy {
 	public abstract class BaseStrategy {
 		protected readonly IExternalService externalService;
-        protected readonly ILogger logger;
-        private readonly IStepManager stepManager;
+		protected readonly ILogger logger;
+		private readonly IStepManager stepManager;
 
 		protected BaseStrategy(IServiceFactory serviceFactory) {
-            this.stepManager = new StepManager(this, serviceFactory);
-            this.logger = serviceFactory.GetLogger();
-            this.externalService = serviceFactory.GetExternalService();
-        }
+			this.stepManager = new StepManager(this, serviceFactory);
+			this.logger = serviceFactory.GetLogger();
+			this.externalService = serviceFactory.GetExternalService();
+		}
 
-        public bool IsSuitableStrategy(AliceRequest request, State state)
-            => Check(request, state ?? new State());
-        
+		public bool IsSuitableStrategy(AliceRequest request, State state)
+			=> Check(request, state ?? new State());
+		
 
-        public Task<AliceResponse> Run(AliceRequest request, State state)
-            =>  CreateResponse(request, state);
-        
+		public Task<AliceResponse> Run(AliceRequest request, State state)
+			=>  CreateResponse(request, state);
+		
 
-        protected virtual async Task<AliceResponse> CreateResponse(AliceRequest request, State state) {
-            try {
-				var simple = await Respond(request, state);
+
+		protected virtual async Task<AliceResponse> CreateResponse(AliceRequest request, State state) {
+			try {
+				var simple = await Respond(request, state); // GetRequestTypes
+
 
 				UpdateState(state);
 
@@ -37,55 +39,55 @@ namespace AliceAppraisal.Engine.Strategy {
 
 				return response;
 			} finally {
-                stepManager.ResetCustomStep();
-            }
-        }
+				stepManager.ResetCustomStep();
+			}
+		}
 
 		protected virtual void UpdateState(State state) {
 			var nextStepName = stepManager.GetNextStep();
-            state.SaveCurrentAndNextStep(this.GetType().FullName, nextStepName);
-        }
+			state.SaveCurrentAndNextStep(this.GetType().FullName, nextStepName);
+		}
 
-        /// <summary>
-        /// Проверка на пригодность стратегии для обработки запроса
-        /// </summary>
-        protected abstract bool Check(AliceRequest request, State state);
-        protected abstract Task<SimpleResponse> Respond(AliceRequest request, State state);
+		/// <summary>
+		/// Проверка на пригодность стратегии для обработки запроса
+		/// </summary>
+		protected abstract bool Check(AliceRequest request, State state);
+		protected abstract Task<SimpleResponse> Respond(AliceRequest request, State state);
 
-        public abstract Task<SimpleResponse> GetMessage(AliceRequest request, State state);
-        public abstract SimpleResponse GetHelp();
-        public abstract SimpleResponse GetMessageForUnknown(AliceRequest request, State state);
+		public abstract Task<SimpleResponse> GetMessage(AliceRequest request, State state);
+		public abstract SimpleResponse GetHelp();
+		public abstract SimpleResponse GetMessageForUnknown(AliceRequest request, State state);
 
 
-        private BaseStrategy GetNextStep(string customNextStep = null) {
-            stepManager.ChangeDefaultStepTo(customNextStep);
-            return stepManager.GetNextStrategy();
-        }
-        /// <summary>
-        /// Вспомогательный метод создания ответа от следующего шага диалога
-        /// </summary>
-        protected Task<SimpleResponse> CreateNextStepMessage(
-            AliceRequest request, 
-            State state, 
-            string customNextStep = null) {
-            var nextStepInstanse = GetNextStep(customNextStep);
-            return nextStepInstanse.GetMessage(request, state);
-        }
+		private BaseStrategy GetNextStep(string customNextStep = null) {
+			stepManager.ChangeDefaultStepTo(customNextStep);
+			return stepManager.GetNextStrategy();
+		}
+		/// <summary>
+		/// Вспомогательный метод создания ответа от следующего шага диалога
+		/// </summary>
+		protected Task<SimpleResponse> CreateNextStepMessage(
+			AliceRequest request, 
+			State state, 
+			string customNextStep = null) {
+			var nextStepInstanse = GetNextStep(customNextStep);
+			return nextStepInstanse.GetMessage(request, state);
+		}
 
-        /// <summary>
-        /// Вспомогательный метод создания ответа от следующего шага диалога
-        /// </summary>
-        protected SimpleResponse CreateNextStepHelp(string customNextStep = null) {
-            var nextStepInstanse = GetNextStep(customNextStep);
-            return nextStepInstanse.GetHelp();
-        }
+		/// <summary>
+		/// Вспомогательный метод создания ответа от следующего шага диалога
+		/// </summary>
+		protected SimpleResponse CreateNextStepHelp(string customNextStep = null) {
+			var nextStepInstanse = GetNextStep(customNextStep);
+			return nextStepInstanse.GetHelp();
+		}
 
-        /// <summary>
-        /// Вспомогательный метод создания ответа от следующего шага диалога
-        /// </summary>
-        protected SimpleResponse CreateNextStepMessageForUnknown(AliceRequest request, State state, string customNextStep = null) {
-            var nextStepInstanse = GetNextStep(customNextStep);
-            return nextStepInstanse.GetMessageForUnknown(request, state);
-        }
-    }
+		/// <summary>
+		/// Вспомогательный метод создания ответа от следующего шага диалога
+		/// </summary>
+		protected SimpleResponse CreateNextStepMessageForUnknown(AliceRequest request, State state, string customNextStep = null) {
+			var nextStepInstanse = GetNextStep(customNextStep);
+			return nextStepInstanse.GetMessageForUnknown(request, state);
+		}
+	}
 }
