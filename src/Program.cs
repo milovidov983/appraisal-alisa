79cbb508a4f651791042f50f32ceccca06ce1163
Milovidov Vasya <milovidov.vasya@gmail.com>
2021-02-22 21:34:15 +0300

    comment

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,7 @@
 namespace AliceAppraisal {
 	class Program {
 		static async Task Main(string[] args) {
+			// AliceAppraisal.EntryPoint.FunctionHandler стартовая функция
 			await Task.Yield();
 		}
 	}
