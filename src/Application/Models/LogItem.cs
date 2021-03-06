a900afdcb6284f98272d06a9519e2ca5d70105fc
Milovidov Vasya <milovidov.vasya@gmail.com>
2021-03-06 16:59:41 +0300

    new log format

--- src/Application/Models/LogItem.cs
+++ src/Application/Models/LogItem.cs
@@ -8,21 +8,31 @@ namespace AliceAppraisal.Application.Models {
 		public DataInfo Data { get; set; }
 		public ErrorInfo Errors { get; set; }
 
-
-		public class DataInfo {
-			public AliceRequest Request { get; set; }
-			public AliceResponse Response { get; set; }
+		public static DataInfo BuildLogItem(AliceRequest request, AliceResponse response) {
+			var item = new DataInfo {
+				Request = new DataInfo.Req {
+					UserRequest = request.Request,
+					Locale = request.Meta.Locale,
+					Timezone = request.Meta.Timezone,
+					ClientId = request.Meta.ClientId,
+					NextAction = request.State.Session.NextAction,
+					PrevAction = request.State.Session.PrevAction
+				},
+				Response = new DataInfo.Resp {
+					MessageId = response.Session.MessageId,
+					AppraisalRequest = response.State.Request,
+					NextAction = response.State.NextAction,
+					PrevAction = response.State.PrevAction,
+					Text = response.Response.Text,
+					UserId = response.Session.UserId
+				}
+			};
+			return item;
 		}
 
-		public class ErrorInfo {
-			public string Message { get; set; }
-		}
 
 		public LogItem(AliceRequest req, AliceResponse resp, Exception ex) {
-			Data = new DataInfo {
-				Request = req,
-				Response = resp
-			};
+			Data = BuildLogItem(req, resp);
 			Errors = ex != null
 				? new ErrorInfo {
 					Message = ex.Message
@@ -32,6 +42,33 @@ namespace AliceAppraisal.Application.Models {
 
 		public static LogItem Create(AliceRequest req, AliceResponse resp, Exception ex) {
 			return new LogItem(req, resp, ex);
+		}
+
+		public class ErrorInfo {
+			public string Message { get; set; }
+		}
+
+
+		public class DataInfo {
+			public Req Request { get; set; }
+			public Resp Response { get; set; }
+
+			public class Req {
+				public string Locale { get; set; }
+				public string Timezone { get; set; }
+				public string ClientId { get; set; }
+				public RequestInfo UserRequest { get; set; }
+				public string PrevAction { get; set; }
+				public string NextAction { get; set; }
+			}
+			public class Resp {
+				public string Text { get; set; }
+				public AppraisalQuoteRequest AppraisalRequest { get; set; }
+				public string UserId { get; set; }
+				public int MessageId { get; set; }
+				public string PrevAction { get; set; }
+				public string NextAction { get; set; }
+			}
 		}
 	}
 }
